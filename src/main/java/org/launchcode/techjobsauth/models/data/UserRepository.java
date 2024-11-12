1274c536fbe24cb1e6e6e3ba66d96631326fdec0
GrizzlyMain <ryan.thom.dalton@gmail.com>
2024-11-11 20:26:09 -0600

    Studio 11/11

--- src/main/java/org/launchcode/techjobsauth/models/data/UserRepository.java
+++ src/main/java/org/launchcode/techjobsauth/models/data/UserRepository.java
@@ -1,13 +1,10 @@
 package org.launchcode.techjobsauth.models.data;
 
-import jakarta.transaction.Transactional;
-import org.launchcode.techjobsauth.models.Job;
 import org.launchcode.techjobsauth.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Transactional
-public interface JobRepository extends CrudRepository<Job, Integer> {
-
+public interface UserRepository extends CrudRepository <User, Integer>{
+    User findByUsername(String username);
 }
